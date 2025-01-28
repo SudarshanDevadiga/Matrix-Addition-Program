12dbf0aa998a64fbe2ccc88a76e65b0212244205
SudarshanDevadiga <sudarshandevadiga200@gmail.com>
2025-01-28 14:23:24 +0530

    Add files via upload

---
+++ matrixOperation.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows and columns: ";
+    cin >> rows >> cols;
+    int matrix1[10][10], matrix2[10][10], result[10][10];
+    cout << "Enter elements of first matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix1[i][j];
+        }
+    }
+    cout << "Enter elements of second matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix2[i][j];
+        }
+    }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+    cout << "Resultant matrix after addition:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
